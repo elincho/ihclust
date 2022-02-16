b33f440f302ff86515b5b3f348dc74f17d61278f
elinny811 <elincho524@gmail.com>
2022-02-15 22:43:18 -0600

    added a dataset "dispensingData"

---
+++ R/dispensingData.R
@@ -0,0 +1,19 @@
+#'Dispensing data.
+#'
+#' A dataset containing estimated opioid dispensing rate
+#' per 100 persons in United States, 2006-2020.
+#'
+#' @format A data frame with 3085 rows and 30 variables:
+#' \describe{
+#'   \item{fips}{FIPS county code}
+#'   \item{State}{State}
+#'   \item{County}{County}
+#'   \item{2006-2020}{estimated opioid dispensing rate per 100 persons in each year}
+#'   \item{POPESTIMATE2010-POPESTIMATE2020}{annual population estimates
+#'   from the US Census Bureau in each year}
+#'
+#'
+#'   ...
+#' }
+#' @source \url{https://www.cdc.gov/drugoverdose/rxrate-maps/index.html}
+"dispensingData"
